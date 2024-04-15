7b15d1530aa2b3259372257ea7cd7c68cf5980ec
realbig4199 <realbig4199@gmail.com>
2024-04-15 10:04:11 +0900

    토큰을 이용해서 요청 보내기

--- index.js
+++ index.js
@@ -31,9 +31,27 @@ app.post("/login", (req, res) => {
   res.json({ accessToken: accessToken });
 });
 
-app.get("/posts", (req, res) => {
+app.get("/posts", authMiddleware, (req, res) => {
   res.json(posts);
 });
+
+function authMiddleware(req, res, next) {
+  // request 헤더에서 토큰 가져오기
+  const authHeader = req.headers["authorization"];
+  // 토큰 파싱 : && 연산자를 이용한 null 체크, 이것을 해주지 않으면 split 함수에서 오류가 발생할 수 있음
+  const token = authHeader && authHeader.split(" ")[1];
+  // 토큰이 없을 경우 : 401 에러(클라이언트에서 잘못된 요청을 보냈을 경우 사용하는 에러 코드)
+  if (token == null) return res.sendStatus(401);
+
+  // 토큰 검증
+  jwt.verify(token, secretText, (err, user) => {
+    // 토큰이 유효하지 않을 경우 : 403 에러(서버에서 요청을 거부했을 경우 사용하는 에러 코드)
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    // 다음 미들웨어로 이동
+    next();
+  });
+}
 
 // express 앱 실행
 const port = 4000;
